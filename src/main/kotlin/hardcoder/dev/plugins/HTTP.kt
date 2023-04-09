23dfeca6c94b62137560a5d59e86a659520f3e72
hardcoderdev <hardcode.dec@gmail.com>
2023-04-09 13:59:43 +0300

    Initial commit

---
+++ src/main/kotlin/hardcoder/dev/plugins/HTTP.kt
@@ -0,0 +1,17 @@
+package hardcoder.dev.plugins
+
+import io.ktor.server.plugins.cachingheaders.*
+import io.ktor.http.content.*
+import io.ktor.http.*
+import io.ktor.server.application.*
+
+fun Application.configureHTTP() {
+    install(CachingHeaders) {
+        options { call, outgoingContent ->
+            when (outgoingContent.contentType?.withoutParameters()) {
+                ContentType.Text.CSS -> CachingOptions(CacheControl.MaxAge(maxAgeSeconds = 24 * 60 * 60))
+                else -> null
+            }
+        }
+    }
+}
